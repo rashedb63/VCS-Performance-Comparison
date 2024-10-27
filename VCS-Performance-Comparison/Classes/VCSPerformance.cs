8345de98951fb2b8a6985bab797f791c688c9f95
Rashed Bahlool <r.bahlool@alsalambank.com>
2024-10-27 21:51:54 +0300

    Initialize Project

---
+++ VCS-Performance-Comparison/Classes/VCSPerformance.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Data;
+
+namespace Git_Repositories_Performance_Comparison.Classes
+{
+    internal class VCSPerformance
+    {
+        public enum VCS
+        {
+            git,
+            mercurial
+        }
+        private VCS VersionControlType;
+        private string TargetFolder;
+        public VCSPerformance() { }
+        public VCSPerformance(VCS versionControlType, string targetFolder)
+        {
+            try
+            {
+                TargetFolder = targetFolder;
+                if (!Directory.Exists(TargetFolder))
+                {
+                    DisplayMessage("Target Directory for the experiment was not found. Creating it...", ConsoleColor.Green);
+                    System.IO.Directory.CreateDirectory(TargetFolder);
+                }
+                else
+                {
+                    DisplayMessage("Target Directory already exists. Clearing the directory...", ConsoleColor.Yellow);
+                    ClearExperimentDirectory(targetFolder);
+                }
+                DisplayMessage("Setting the version control to \"" + versionControlType + "\"...", ConsoleColor.Green);
+                VersionControlType = versionControlType;
+            }
+            catch(Exception ex)
+            {
+                DisplayMessage("Error creating the experimentation directory and/or setting the version control type!", ConsoleColor.Red);
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+        public void SetExperimentDirectory(string targetFolder)
+        {
+            TargetFolder = targetFolder;
+            if (!Directory.Exists(TargetFolder))
+            {
+                System.IO.Directory.CreateDirectory(TargetFolder);
+            }
+        }
+        public string getExperimentDirectory()
+        {
+            return TargetFolder;
+        }
+        public void SetVersionControlType(VCS versionControlType)
+        {
+            VersionControlType = versionControlType;
+        }
+        public VCS GetVersionControlType()
+        {
+            return VersionControlType;
+        }
+        public void InitializeRepository()
+        {
+            DisplayMessage("Initializing a new \"" + VersionControlType + "\" repository in the target directory...", ConsoleColor.White);
+            Process process = new Process();
+            process.StartInfo.FileName = @"cmd.exe";
+            process.StartInfo.Arguments = VersionControlType == VCS.git ? "/c cd \"" + TargetFolder + "\" && git init" : "/c cd \"" + TargetFolder + "\" && hg init";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.Start();
+            process.WaitForExit();
+            DisplayMessage("\"" + VersionControlType + "\" repository has been successfully created in the target directory...", ConsoleColor.Green);
+        }
+        private void ClearExperimentDirectory(string targetFolder)
+        {
+            try
+            {
+                System.IO.DirectoryInfo di = new DirectoryInfo(targetFolder);
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to clear the experimentation folder caused by: ");
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+        private void DisplayMessage(string Message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(Message);
+        }
+    }
+}
---
+++ VCS-Performance-Comparison/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Git_Repositories_Performance_Comparison.Classes;
+
+namespace VCS_Performance_Comparison
+{
+    internal class Program
+    {
+        // Specify the target folder for the experimentation
+        const string TargetLocation = @"C:\Users\rashe\OneDrive\Desktop\Experiment";
+        static void Main(string[] args)
+        {
+            // Initialize the class to use or create the folder if doesn't exist
+            VCSPerformance performance = new VCSPerformance(VCSPerformance.VCS.mercurial, TargetLocation);
+            performance.InitializeRepository();
+            //long memoryUsage;
+            //float cpuUsage;
+            //TimeSpan cpuTime;
+            //PerformanceCounter cpuCounter;
+
+            //Process process = new Process();
+            //process.StartInfo.FileName = @"cmd.exe";
+            //process.StartInfo.Arguments = "/c cd Desktop"; // Note the /c command (*)
+            //process.StartInfo.UseShellExecute = false;
+            //process.StartInfo.RedirectStandardOutput = true;
+            //process.StartInfo.RedirectStandardError = true;
+            //process.Start();
+            //////* Read the output (or the error)
+            ////string output = process.StandardOutput.ReadToEnd();
+            ////Console.WriteLine(output);
+            ////string err = process.StandardError.ReadToEnd();
+            ////Console.WriteLine(err);
+            ////// Monitor resource usage
+            //while (!process.HasExited)
+            //{
+            //    // Get memory and CPU usage
+            //    memoryUsage = process.WorkingSet64; // Memory in bytes
+            //    cpuTime = process.TotalProcessorTime;
+            //    cpuCounter = new PerformanceCounter("Process", "% Processor Time", Process.GetProcessById(process.Id).ProcessName);
+
+            //    // Get CPU usage after process execution
+            //    cpuUsage = cpuCounter.NextValue();
+
+            //    // Output the results
+            //    Console.WriteLine($"CPU Usage: {cpuUsage}%");
+
+            //    // Wait for a short interval before checking again
+            //    //Thread.Sleep(500);
+            //}
+
+            //// Final resource usage after the process exits
+            //long finalMemoryUsage = process.WorkingSet64;
+            //TimeSpan finalCpuTime = process.TotalProcessorTime;
+
+            //Console.WriteLine($"Final Memory Usage: {finalMemoryUsage / 1024} KB");
+            //Console.WriteLine($"Final CPU Time: {finalCpuTime.TotalMilliseconds} ms");
+
+            //process.WaitForExit(); // Optional: Wait for the process to exit completely
+
+            Console.Read();
+        }
+    }
+}
